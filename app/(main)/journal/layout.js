881499ef42ee4046c0b24ea40ac8f44f6e16fb65
KARTHIKEYAN R <karthikeyanr2703@gmail.com>
2025-03-14 22:57:33 +0530

    made changes

--- app/(main)/journal/layout.js
+++ app/(main)/journal/layout.js
@@ -1,19 +1,22 @@
 import { MoveLeft } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 
 const JournalPage = ({ children }) => {
   return (
-    <div className="w-full">
-      <div className="w-full flex items-center justify-start">
-        <Link
-          href="/dashboard"
-          className="flex flex-row items-center justify-center px-3 "
-        >
-          <MoveLeft className="mr-2" /> Back to Dashboard
-        </Link>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="w-full">
+        <div className="w-full flex items-center justify-start">
+          <Link
+            href="/dashboard"
+            className="flex flex-row items-center justify-center px-3 "
+          >
+            <MoveLeft className="mr-2" /> Back to Dashboard
+          </Link>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Suspense>
   );
 };
 
